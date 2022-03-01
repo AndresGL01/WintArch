2091f4a7aed9af689f21dabf06007dcc95d1af38
AndresGL01 <andreslp0001@gmail.com>
2022-03-01 17:19:02 +0100

    First Commit

---
+++ Scripts/Environment/execute.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+
+# Set a custom Wallpaper
+sudo feh --bg-scale /home/winters/ArchConfig/Wallpapers/ARCH_Orange.png
---
+++ Scripts/Environment/minimal_packages.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+pacman -S rofi zsh pavucontrol pulseaudio pamixer brightnessctl ttf-fira-code feh 
