1f33ed7ed9e0918ecbade1c4b934aa35a4081586
AndresGL01 <andreslp0001@gmail.com>
2022-03-04 17:21:49 +0100

    Added more packages

--- scripts/visual_packages.sh
+++ scripts/visual_packages.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-sudo pacman -S xorg-xinit qtile rofi pavucontrol pulseaudio pamixer brightnessctl ttf-fira-code feh volumeicon cbatticon
+sudo pacman -S xorg-xinit xorg lightdm lightdm-gtk-greeter  qtile rofi pavucontrol pulseaudio pamixer brightnessctl ttf-fira-code feh volumeicon cbatticon
