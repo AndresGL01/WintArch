bb40e2f16a996969231fbd9f9f7bb12bcbb14647
Andres <andreslp0001@gmail.com>
2022-03-17 12:14:48 +0100

    Added zsh to setup

--- scripts/replace_bash_to_zsh.sh
+++
@@ -1,9 +0,0 @@
-# !bin/bash
-
-# Check if zsh is installed
-sudo pacman -S zsh; sudo pacman -Syu zsh
-
-# First set root zsh
-sudo chsh -s /usr/bin/zsh
-# After set user zsh
-chsh -s /usr/bin/zsh
--- setups/setup_2.sh
+++ setups/setup_2.sh
@@ -16,3 +16,20 @@ cd mishmash_tools
 cd programming_tools
 
 ./programming_ides.sh
+
+# Moving to installer folder
+
+cd ..
+
+# Change bash -> zsh
+
+cd qtile
+./replace_bash_to_zsh.sh
+
+# Moving to config folder
+
+cd $HOME/config
+
+# Cloning ohmyzsh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
